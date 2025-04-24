5b139361f3d5a1346890a222f3af1371d2e1199a
erikgalva <info@erikgalvagni.com>
2025-04-24 10:57:02 +0200

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,9 +1,14 @@
-export const supabase = createBrowserClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    db: {
-      schema: 'basejump',
-    },
-  }
-);
+import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "@/types/supabase";
+
+export const createClient = () => {
+  return createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      db: {
+        schema: "basejump", // 👈 importante
+      },
+    }
+  );
+};
